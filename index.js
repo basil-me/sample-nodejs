92829845b5f263135b65a243ada07d065b4a726c
basil <basil68@ymail.com>
2022-01-24 12:30:04 +0530

    indexupdated

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Basil m jacob" }');
+    res.send('{ "Response": "Hello From Basil m jacob Using NodeJs APP" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
-});
-app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
-});
-app.listen(process.env.PORT || 3000);
-module.exports = app;
